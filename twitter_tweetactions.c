29267e9839fc430d2d3a2270eeebd816522da1c3
neaveru <neaveru@gmail.com>
2010-03-03 21:44:04 -0500

    Added icon to end of tweets for chats

--- twitter_tweetactions.c
+++ twitter_tweetactions.c
@@ -3,6 +3,18 @@
 
 #include "twitter_conn.h"
 
+//TODO: 3rd param should be tweet data
+static gboolean twitter_tweet_action_icon_event(GtkWidget *w, GdkEvent *event, GtkIMHtml *imhtml)
+{
+	if (event->type == GDK_ENTER_NOTIFY)
+	{
+		gdk_window_set_cursor(w->window, imhtml->hand_cursor);
+	} else if (event->type == GDK_BUTTON_RELEASE) {
+		//TODO: add actions
+	}
+	return FALSE;
+}
+
 static void twitter_tweet_actions_displayed_chat_cb(PurpleAccount *account, const char *who, char *message,
 		PurpleConversation *conv,
 		PurpleMessageFlags flags,
@@ -11,6 +23,7 @@ static void twitter_tweet_actions_displayed_chat_cb(PurpleAccount *account, cons
 	GtkIMHtml *imhtml;
 	GtkTextBuffer *text_buffer;
 	GtkTextIter iter;
+	GtkWidget *box, *img;
 
 	if (account != account_signal)
 		return;
@@ -19,6 +32,23 @@ static void twitter_tweet_actions_displayed_chat_cb(PurpleAccount *account, cons
 	text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(imhtml));
 
 	gtk_text_buffer_get_end_iter(text_buffer, &iter);
+
+	box = gtk_event_box_new();
+	//TODO: cache this?
+	//TODO: text version
+	//TODO: change this image to something relevant
+	img = gtk_image_new_from_stock(GTK_STOCK_PROPERTIES, GTK_ICON_SIZE_MENU);
+
+	gtk_container_add(GTK_CONTAINER(box), GTK_WIDGET(img));
+
+	gtk_text_view_add_child_at_anchor(GTK_TEXT_VIEW(imhtml), box,
+			gtk_text_buffer_create_child_anchor(text_buffer, &iter));
+
+	gtk_widget_show(box);
+	gtk_widget_show(img);
+
+	//TODO: need to pass tweet data here
+	g_signal_connect(G_OBJECT(box), "event", G_CALLBACK(twitter_tweet_action_icon_event), imhtml);
 
 }
 
