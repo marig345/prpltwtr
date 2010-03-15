5cbf6732a6dbb90a0a1199322a0380b54710dd3d
neaveru <neaveru@gmail.com>
2010-03-15 14:52:31 -0400

    Updated AUTHORS file

---
+++ twitter_endpoint_chat.c
@@ -0,0 +1,502 @@
+#include "twitter_endpoint_chat.h"
+#include "twitter_convicon.h"
+#include "twitter_buddy.h"
+
+static gint twitter_get_next_chat_id()
+{
+	static gint chat_id = 1;
+	return chat_id++;
+}
+
+void twitter_endpoint_chat_free(TwitterEndpointChat *ctx)
+{
+	PurpleConnection *gc;
+	GList *l;
+
+	if (ctx->settings && ctx->settings->endpoint_data_free)
+		ctx->settings->endpoint_data_free(ctx->endpoint_data);
+	gc = purple_account_get_connection(ctx->account);
+
+	if (ctx->timer_handle)
+	{
+		purple_timeout_remove(ctx->timer_handle);
+		ctx->timer_handle = 0;
+	}
+	if (ctx->chat_name)
+	{
+		g_free(ctx->chat_name);
+		ctx->chat_name = NULL;
+	}
+
+	for (l = ctx->sent_tweet_ids; l; l = l->next)
+		g_free(l->data);
+	g_list_free(ctx->sent_tweet_ids);
+	g_slice_free(TwitterEndpointChat, ctx);
+}
+
+TwitterEndpointChat *twitter_endpoint_chat_new(
+	TwitterEndpointChatSettings *settings,
+	TwitterChatType type, PurpleAccount *account, const gchar *chat_name,
+	GHashTable *components)
+{
+	TwitterEndpointChat *ctx = g_slice_new0(TwitterEndpointChat);
+	ctx->settings = settings;
+	ctx->type = type;
+	ctx->account = account;
+	ctx->chat_name = g_strdup(chat_name);
+	ctx->endpoint_data = settings->create_endpoint_data ?
+		settings->create_endpoint_data(components) :
+		NULL;
+
+	return ctx;
+}
+TwitterEndpointChatId *twitter_endpoint_chat_id_new(TwitterEndpointChat *chat)
+{
+	TwitterEndpointChatId *chat_id;
+	g_return_val_if_fail(chat != NULL, NULL);
+
+	chat_id = g_slice_new0(TwitterEndpointChatId);
+	chat_id->account = chat->account;
+	chat_id->chat_name = g_strdup(chat->chat_name);
+	return chat_id;
+}
+void twitter_endpoint_chat_id_free(TwitterEndpointChatId *chat_id)
+{
+	if (chat_id == NULL)
+		return;
+	g_free(chat_id->chat_name);
+	chat_id->chat_name = NULL;
+
+	g_slice_free(TwitterEndpointChatId, chat_id);
+}
+TwitterEndpointChat *twitter_endpoint_chat_find_by_id(TwitterEndpointChatId *chat_id)
+{
+	g_return_val_if_fail(chat_id != NULL, NULL);
+	return twitter_endpoint_chat_find(chat_id->account, chat_id->chat_name);
+}
+
+static PurpleConversation *twitter_endpoint_chat_find_open_conv(TwitterEndpointChat *ctx)
+{
+#if _HAZE_
+	return purple_find_conversation_with_account(PURPLE_CONV_TYPE_IM, ctx->chat_name, ctx->account);
+#else
+	return purple_find_conversation_with_account(PURPLE_CONV_TYPE_CHAT, ctx->chat_name, ctx->account);
+#endif
+}
+
+//Taken mostly from blist.c
+//XXX
+static PurpleChat *_twitter_blist_chat_find(PurpleAccount *account, TwitterChatType chat_type,
+	const char *component_key, const char *component_value)
+{
+	const char *node_chat_name;
+	gint node_chat_type = 0;
+	const char *node_chat_type_str;
+	PurpleChat *chat;
+	PurpleBlistNode *node, *group;
+	char *normname;
+	PurpleBuddyList *purplebuddylist = purple_get_blist();
+	GHashTable *components;
+
+	g_return_val_if_fail(purplebuddylist != NULL, NULL);
+	g_return_val_if_fail((component_value != NULL) && (*component_value != '\0'), NULL);
+
+	normname = g_strdup(purple_normalize(account, component_value));
+	purple_debug_info(TWITTER_PROTOCOL_ID, "Account %s searching for chat %s type %d\n",
+		account->username,
+		component_value == NULL ? "NULL" : component_value,
+		chat_type);
+
+	if (normname == NULL)
+	{
+		return NULL;
+	}
+	for (group = purplebuddylist->root; group != NULL; group = group->next) {
+		for (node = group->child; node != NULL; node = node->next) {
+			if (PURPLE_BLIST_NODE_IS_CHAT(node)) {
+
+				chat = (PurpleChat*)node;
+
+				if (account != chat->account)
+					continue;
+
+				components = purple_chat_get_components(chat);
+				if (components != NULL)
+				{
+					node_chat_type_str = g_hash_table_lookup(components, "chat_type");
+					node_chat_name = g_hash_table_lookup(components, component_key);
+					node_chat_type = node_chat_type_str == NULL ? 0 : strtol(node_chat_type_str, NULL, 10);
+
+					if (node_chat_name != NULL && node_chat_type == chat_type && !strcmp(purple_normalize(account, node_chat_name), normname))
+					{
+						g_free(normname);
+						return chat;
+					}
+				}
+			}
+		}
+	}
+
+	g_free(normname);
+	return NULL;
+}
+
+//XXX
+PurpleChat *twitter_blist_chat_find_search(PurpleAccount *account, const char *name)
+{
+	return _twitter_blist_chat_find(account, TWITTER_CHAT_SEARCH, "search", name);
+}
+
+//XXX
+PurpleChat *twitter_blist_chat_find_timeline(PurpleAccount *account, gint timeline_id)
+{
+	char *tmp = g_strdup_printf("%d", timeline_id);
+	PurpleChat *chat = _twitter_blist_chat_find(account, TWITTER_CHAT_TIMELINE, "timeline_id", tmp);
+	g_free(tmp);
+	return chat;
+}
+
+//XXX TODO: fix me
+PurpleChat *twitter_blist_chat_find(PurpleAccount *account, const char *name)
+{
+	static char *timeline = "Timeline: ";
+	static char *search = "Search: ";
+	PurpleChat *c = NULL;
+	if (strlen(name) > strlen(timeline) && !strncmp(timeline, name, strlen(timeline)))
+	{
+		c = twitter_blist_chat_find_timeline(account, 0);
+	} else if (strlen(name) > strlen(search) && !strncmp(search, name, strlen(search))) {
+		c = twitter_blist_chat_find_search(account, name + strlen(search));
+	} else {
+		c = twitter_blist_chat_find_search(account, name);
+	}
+	return c;
+}
+
+//TODO should be static?
+gboolean twitter_blist_chat_is_auto_open(PurpleChat *chat)
+{
+	g_return_val_if_fail(chat != NULL, FALSE);
+	GHashTable *components = purple_chat_get_components(chat);
+	char *auto_open = g_hash_table_lookup(components, "auto_open");
+	return (auto_open != NULL && auto_open[0] != '0');
+}
+
+static void twitter_chat_add_tweet(PurpleConversation *conv, const char *who, const char *message, long long id, time_t time)
+{
+	gchar *tweet;
+	purple_debug_info(TWITTER_PROTOCOL_ID, "%s\n", G_STRFUNC);
+#if !_HAZE_
+	PurpleConvChat *chat;
+	chat = purple_conversation_get_chat_data(conv);
+	g_return_if_fail(chat != NULL);
+#endif
+	g_return_if_fail(conv != NULL);
+	g_return_if_fail(who != NULL);
+	g_return_if_fail(message != NULL);
+
+	tweet = twitter_format_tweet(
+			purple_conversation_get_account(conv),
+			who,
+			message,
+			id,
+			PURPLE_CONV_TYPE_CHAT,
+			purple_conversation_get_name(conv),
+			TRUE);
+#if _HAZE_
+	//This isn't in twitter_Format_tweet because we can't distinguish between a im and a chat
+	gchar *tweet2 = g_strdup_printf("%s: %s", who, tweet);
+	g_free(tweet);
+	tweet = tweet2;
+	serv_got_im(purple_conversation_get_gc(conv), conv->name, tweet, PURPLE_MESSAGE_RECV, time);
+#else
+	if (!purple_conv_chat_find_user(chat, who))
+	{
+		purple_debug_info(TWITTER_PROTOCOL_ID, "added %s to chat %s\n",
+				who,
+				purple_conversation_get_name(conv));
+		purple_conv_chat_add_user(chat,
+				who,
+				NULL,   /* user-provided join message, IRC style */
+				PURPLE_CBFLAGS_NONE,
+				FALSE);  /* show a join message */
+	}
+	purple_debug_info(TWITTER_PROTOCOL_ID, "message %s\n", message);
+	serv_got_chat_in(purple_conversation_get_gc(conv),
+			purple_conv_chat_get_id(chat),
+			who,
+			PURPLE_MESSAGE_RECV,
+			tweet,
+			time);
+#endif
+	g_free(tweet);
+}
+
+static PurpleConversation *twitter_endpoint_chat_get_conv(TwitterEndpointChat *endpoint_chat)
+{
+#if _HAZE_
+	return twitter_endpoint_chat_find_open_conv(endpoint_chat);
+#else
+	PurpleConversation *conv = twitter_endpoint_chat_find_open_conv(endpoint_chat);
+	PurpleChat *blist_chat;
+	if (conv == NULL && (blist_chat = twitter_blist_chat_find(endpoint_chat->account, endpoint_chat->chat_name)))
+	{
+		if (twitter_blist_chat_is_auto_open(blist_chat))
+		{
+			purple_debug_info(TWITTER_PROTOCOL_ID, "%s, recreated conv for auto open chat (%s)\n", G_STRFUNC, endpoint_chat->chat_name);
+			guint chat_id = twitter_get_next_chat_id();
+			conv = serv_got_joined_chat(purple_account_get_connection(endpoint_chat->account), chat_id, endpoint_chat->chat_name);
+		}
+	}
+	return conv;
+#endif
+}
+
+
+void twitter_chat_got_tweet(TwitterEndpointChat *endpoint_chat, TwitterUserTweet *tweet)
+{
+	PurpleConversation *conv = twitter_endpoint_chat_get_conv(endpoint_chat);
+	g_return_if_fail(conv != NULL);
+	g_return_if_fail(tweet != NULL);
+	g_return_if_fail(tweet->screen_name != NULL);
+	g_return_if_fail(tweet->status != NULL);
+
+#if _HAVE_PIDGIN_
+	//TODO: make this into a signal?
+	twitter_conv_icon_got_user_icon(purple_conversation_get_account(conv),
+			tweet->screen_name, tweet->icon_url, tweet->status->created_at);
+#endif
+	twitter_chat_add_tweet(conv, tweet->screen_name, tweet->status->text, tweet->status->id, tweet->status->created_at);
+}
+
+static gboolean twitter_sent_tweets_contains_id(TwitterEndpointChat *ctx, long long id)
+{
+	GList *l;
+	for (l = ctx->sent_tweet_ids; l; l = l->next)
+	{
+		long long *el = l->data;
+		if (*el == id)
+			return TRUE;
+		else if (*el > id)
+			return FALSE;
+	}
+	return FALSE;
+}
+
+//Removes all tweet id before id
+static void twitter_sent_tweets_ids_remove_before(TwitterEndpointChat *ctx, long long id)
+{
+	while (ctx->sent_tweet_ids && *((long long *) ctx->sent_tweet_ids->data) <= id)
+	{
+		g_free(ctx->sent_tweet_ids->data);
+		ctx->sent_tweet_ids = g_list_delete_link(ctx->sent_tweet_ids, ctx->sent_tweet_ids);
+	}
+}
+
+void twitter_chat_got_user_tweets(TwitterEndpointChat *endpoint_chat, GList *user_tweets)
+{
+	PurpleAccount *account;
+	GList *l;
+	long long max_id = 0;
+
+	g_return_if_fail(endpoint_chat != NULL);
+
+	account = endpoint_chat->account;
+
+	if (!user_tweets)
+		return;
+
+	l = g_list_last(user_tweets);
+	max_id = ((TwitterUserTweet *) l->data)->status->id;
+	for (l = user_tweets; l; l = l->next)
+	{
+		TwitterUserTweet *user_tweet = l->data;
+		TwitterUserData *user = twitter_user_tweet_take_user_data(user_tweet);
+		TwitterTweet *status;
+
+		if (user)
+			twitter_buddy_set_user_data(account, user, FALSE);
+
+		/* This could be more efficient */
+		if (!twitter_sent_tweets_contains_id(endpoint_chat, user_tweet->status->id))
+			twitter_chat_got_tweet(endpoint_chat, user_tweet);
+
+		status = twitter_user_tweet_take_tweet(user_tweet);
+		twitter_buddy_set_status_data(account, user_tweet->screen_name, status);
+
+		twitter_user_tweet_free(user_tweet);
+	}
+	twitter_sent_tweets_ids_remove_before(endpoint_chat, max_id);
+	g_list_free(user_tweets);
+}
+
+static int _tweet_id_compare(long long *a, long long *b)
+{
+	if (*a < *b)
+		return -1;
+	else if (*a == *b)
+		return 0;
+	else
+		return 1;
+}
+
+static void twitter_add_sent_tweet_id(TwitterEndpointChat *endpoint_chat, long long tweet_id)
+{
+	long long *p = g_new(long long, 1);
+	*p = tweet_id;
+	endpoint_chat->sent_tweet_ids = g_list_insert_sorted(endpoint_chat->sent_tweet_ids,
+			p,
+			(GCompareFunc) _tweet_id_compare);
+}
+
+static gboolean twitter_endpoint_chat_interval_timeout(gpointer data)
+{
+	TwitterEndpointChat *endpoint = data;
+	if (endpoint->settings->interval_timeout)
+		return endpoint->settings->interval_timeout(endpoint);
+	return FALSE;
+}
+
+
+void twitter_endpoint_chat_start(PurpleConnection *gc, TwitterEndpointChatSettings *settings,
+		GHashTable *components, gboolean open_conv) 
+{
+        const char *interval_str = g_hash_table_lookup(components, "interval");
+        int interval = 0;
+
+	g_return_if_fail(settings != NULL);
+
+        interval = interval_str == NULL ? 0 : strtol(interval_str, NULL, 10);
+
+	PurpleAccount *account = purple_connection_get_account(gc);
+        int default_interval = settings->get_default_interval(account);
+	gchar *error = NULL;
+
+	if (settings->verify_components && (error = settings->verify_components(components)))
+	{
+		purple_notify_info(gc,  /* plugin handle or PurpleConnection */
+				("Chat Join"),
+				("Invalid Chat"),
+				(error));
+		g_free(error);
+		return;
+	}
+
+        if (interval < 1)
+                interval = default_interval;
+
+	char *chat_name = settings->get_name(components);
+
+#if _HAZE_
+	//HAZE only works when the conv has already been created
+	//as opposed to right before the conversation has been created
+        if (purple_find_conversation_with_account(PURPLE_CONV_TYPE_IM, chat_name, account)) {
+#else
+        if (!purple_find_conversation_with_account(PURPLE_CONV_TYPE_CHAT, chat_name, account)) {
+#endif
+		if (!twitter_endpoint_chat_find(account, chat_name))
+		{
+			TwitterConnectionData *twitter = gc->proto_data;
+			TwitterEndpointChat *endpoint_chat = twitter_endpoint_chat_new(
+					settings, settings->type, account, chat_name, components);
+			g_hash_table_insert(twitter->chat_contexts,
+					g_strdup(purple_normalize(account, chat_name)),
+					endpoint_chat);
+			settings->on_start(endpoint_chat);
+
+			endpoint_chat->timer_handle = purple_timeout_add_seconds(
+					60 * interval,
+					twitter_endpoint_chat_interval_timeout, endpoint_chat);
+		}
+#if !_HAZE_
+		if (open_conv)
+		{
+			guint chat_id = twitter_get_next_chat_id();
+			serv_got_joined_chat(gc, chat_id, chat_name);
+		}
+#endif
+        } else {
+                purple_debug_info(TWITTER_PROTOCOL_ID, "Chat %s is already open.", chat_name);
+        }
+	g_free(chat_name);
+}
+
+TwitterEndpointChat *twitter_endpoint_chat_find(PurpleAccount *account, const char *chat_name)
+{
+	PurpleConnection *gc = purple_account_get_connection(account);
+	TwitterConnectionData *twitter = gc->proto_data;
+	return (TwitterEndpointChat *) g_hash_table_lookup(twitter->chat_contexts, purple_normalize(account, chat_name));
+}
+
+static void twitter_endpoint_chat_send_success_cb(PurpleAccount *account, xmlnode *node, gboolean last, gpointer _ctx_id)
+{
+	TwitterEndpointChatId *id = _ctx_id;
+	TwitterUserTweet *user_tweet = twitter_update_status_node_parse(node);
+	TwitterTweet *tweet = user_tweet ? user_tweet->status : NULL;
+	TwitterEndpointChat *ctx = twitter_endpoint_chat_find_by_id(id);
+
+#if !_HAZE_
+	PurpleConversation *conv;
+
+	if (ctx && tweet && tweet->text && (conv = twitter_endpoint_chat_find_open_conv(ctx)))
+	{
+#if _HAVE_PIDGIN_
+		twitter_conv_icon_got_user_icon(account,
+				user_tweet->screen_name, user_tweet->icon_url, user_tweet->status->created_at);
+#endif
+		twitter_chat_add_tweet(conv, account->username, tweet->text, tweet->id, tweet->created_at);
+	}
+
+#endif
+	if (tweet && tweet->id)
+		twitter_add_sent_tweet_id(ctx, tweet->id);
+
+	if (user_tweet)
+		twitter_user_tweet_free(user_tweet);
+
+	if (last)
+		twitter_endpoint_chat_id_free(id);
+}
+
+static gboolean twitter_endpoint_chat_send_error_cb(PurpleAccount *account, const TwitterRequestErrorData *error, gpointer _ctx_id)
+{
+	TwitterEndpointChatId *id = _ctx_id;
+	TwitterEndpointChat *ctx = twitter_endpoint_chat_find_by_id(id);
+	PurpleConversation *conv;
+	if (ctx)
+	{
+		conv = twitter_endpoint_chat_find_open_conv(ctx);
+
+		if (conv)
+		{
+			purple_conversation_write(conv, NULL, "Error sending tweet", PURPLE_MESSAGE_ERROR, time(NULL));
+		}
+	}
+
+	twitter_endpoint_chat_id_free(id);
+
+	return FALSE; //give up trying
+}
+
+int twitter_endpoint_chat_send(TwitterEndpointChat *ctx, const gchar *message)
+{
+	PurpleAccount *account = ctx->account;
+	TwitterEndpointChatId *id;
+	gchar *added_text = NULL;
+
+	if (ctx->settings->get_status_added_text)
+		added_text = ctx->settings->get_status_added_text(ctx);
+
+	GArray *statuses = twitter_utf8_get_segments(message, MAX_TWEET_LENGTH, added_text);
+	id = twitter_endpoint_chat_id_new(ctx);
+	twitter_api_set_statuses(purple_account_get_requestor(account),
+			statuses,
+			0,
+			twitter_endpoint_chat_send_success_cb,
+			twitter_endpoint_chat_send_error_cb,
+			id);
+
+	if (added_text)
+		g_free(added_text);
+	return 0;
+}
---
+++ twitter_util.c
@@ -0,0 +1,356 @@
+/**
+ * Copyright (C) 2009 Tan Miaoqing
+ * Contact: Tan Miaoqing <rabbitrun84@gmail.com>
+ */
+
+#include "twitter_util.h"
+#include "twitter_conn.h"
+#include <version.h>
+
+#if !PURPLE_VERSION_CHECK(2, 6, 0)
+
+//from libpurple/util.c
+static void append_escaped_text(GString *str,
+		const gchar *text, gssize length)
+{
+	const gchar *p;
+	const gchar *end;
+	gunichar c;
+
+	p = text;
+	end = text + length;
+
+	while (p != end)
+	{
+		const gchar *next;
+		next = g_utf8_next_char (p);
+
+		switch (*p)
+		{
+			case '&':
+				g_string_append (str, "&amp;");
+				break;
+
+			case '<':
+				g_string_append (str, "&lt;");
+				break;
+
+			case '>':
+				g_string_append (str, "&gt;");
+				break;
+
+			case '"':
+				g_string_append (str, "&quot;");
+				break;
+
+			default:
+				c = g_utf8_get_char (p);
+				if ((0x1 <= c && c <= 0x8) ||
+						(0xb <= c && c <= 0xc) ||
+						(0xe <= c && c <= 0x1f) ||
+						(0x7f <= c && c <= 0x84) ||
+						(0x86 <= c && c <= 0x9f))
+					g_string_append_printf (str, "&#x%x;", c);
+				else
+					g_string_append_len (str, p, next - p);
+				break;
+		}
+
+		p = next;
+	}
+}
+
+//from libpurple/util.c
+static gchar *purple_markup_escape_text(const gchar *text, gssize length)
+{
+	GString *str;
+
+	g_return_val_if_fail(text != NULL, NULL);
+
+	if (length < 0)
+		length = strlen(text);
+
+	/* prealloc at least as long as original text */
+	str = g_string_sized_new(length);
+	append_escaped_text(str, text, length);
+
+	return g_string_free(str, FALSE);
+}
+#endif
+
+gboolean twitter_usernames_match(PurpleAccount *account, const gchar *u1, const gchar *u2)
+{
+	gboolean match;
+	gchar *u1n = g_strdup(purple_normalize(account, u1));
+	const gchar *u2n = purple_normalize(account, u2);
+	match = !strcmp(u1n, u2n);
+
+	g_free(u1n);
+	return match;
+}
+
+
+long long purple_account_get_long_long(PurpleAccount *account, const gchar *key, long long default_value)
+{
+	const char* tmp_str;
+
+	tmp_str = purple_account_get_string(account, key, NULL);
+	if(tmp_str)
+		return strtoll(tmp_str, NULL, 10);
+	else
+		return default_value;
+}
+
+void purple_account_set_long_long(PurpleAccount *account, const gchar *key, long long value)
+{
+	gchar* tmp_str;
+
+	tmp_str = g_strdup_printf("%lld", value);
+	purple_account_set_string(account, key, tmp_str);
+	g_free(tmp_str);
+}
+
+
+//TODO: move those
+#if _HAVE_PIDGIN_ && PURPLE_VERSION_CHECK(2, 6, 0)
+static const char *_find_first_delimiter(const char *text, const char *delimiters, int *delim_id)
+{
+	const char *delimiter;
+	if (text == NULL || text[0] == '\0')
+		return NULL;
+	do
+	{
+		for (delimiter = delimiters; *delimiter != '\0'; delimiter++)
+		{
+			if (*text == *delimiter)
+			{
+				if (delim_id != NULL)
+					*delim_id = delimiter - delimiters;
+				return text;
+			}
+		}
+	} while (*++text != '\0');
+	return NULL;
+}
+
+static void _g_string_append_escaped_len(GString *s, const gchar *txt, gssize len)
+{
+	gchar *tmp = purple_markup_escape_text(txt, len);
+	g_string_append(s, tmp);
+	g_free(tmp);
+}
+
+//TODO: move those
+static char *twitter_linkify(PurpleAccount *account, const char *message)
+{
+	GString *ret;
+	static char symbols[] = "#@";
+	static char *symbol_actions[] = {TWITTER_URI_ACTION_SEARCH, TWITTER_URI_ACTION_USER};
+	static char delims[] = " :"; //I don't know if this is how I want to do this...
+	const char *ptr = message;
+	const char *end = message + strlen(message);
+	const char *delim = NULL;
+	g_return_val_if_fail(message != NULL, NULL);
+
+	ret = g_string_new("");
+
+	while (ptr != NULL && ptr < end)
+	{
+		const char *first_token;
+		char *current_action;
+		char *link_text;
+		int symbol_index = 0;
+		first_token = _find_first_delimiter(ptr, symbols, &symbol_index);
+		if (first_token == NULL)
+		{
+			_g_string_append_escaped_len(ret, ptr, -1);
+			break;
+		}
+		current_action = symbol_actions[symbol_index];
+		_g_string_append_escaped_len(ret, ptr, first_token - ptr);
+		ptr = first_token;
+		delim = _find_first_delimiter(ptr, delims, NULL);
+		if (delim == NULL)
+			delim = end;
+		link_text = g_strndup(ptr, delim - ptr);
+		//Added the 'a' before the account name because of a highlighting issue... ugly hack
+		g_string_append_printf(ret, "<a href=\"" TWITTER_URI ":///%s?account=a%s&text=%s\">",
+				current_action,
+				purple_account_get_username(account),
+				purple_url_encode(link_text));
+		_g_string_append_escaped_len(ret, link_text, -1);
+		g_string_append(ret, "</a>");
+		ptr = delim;
+
+		g_free(link_text);
+	}
+
+	return g_string_free(ret, FALSE);
+}
+#endif
+
+//TODO: move those
+char *twitter_format_tweet(PurpleAccount *account,
+		const char *src_user,
+		const char *message,
+		long long tweet_id,
+		PurpleConversationType conv_type,
+		const gchar *conv_name,
+		gboolean is_tweet)
+{
+	char *linkified_message;
+	GString *tweet;
+
+#if _HAVE_PIDGIN_ && PURPLE_VERSION_CHECK(2, 6, 0)
+	linkified_message = twitter_linkify(account, message);
+#else
+	linkified_message = purple_markup_escape_text(message, -1);
+#endif
+
+	g_return_val_if_fail(linkified_message != NULL, NULL);
+	g_return_val_if_fail(src_user != NULL, NULL);
+
+	tweet = g_string_new(linkified_message);
+
+#if _HAVE_PIDGIN_ && PURPLE_VERSION_CHECK(2, 6, 0)
+	if (is_tweet && tweet_id && conv_type != PURPLE_CONV_TYPE_UNKNOWN && conv_name)
+	{
+		const gchar *account_name = purple_account_get_username(account);
+		//TODO: make this an image
+		g_string_append_printf(tweet,
+				" <a href=\"" TWITTER_URI ":///" TWITTER_URI_ACTION_ACTIONS "?account=a%s&user=%s&id=%lld",
+				account_name,
+				purple_url_encode(src_user),
+				tweet_id);
+		g_string_append_printf(tweet,
+				"&conv_type=%d&conv_name=%s\">*</a>",
+				conv_type,
+				purple_url_encode(conv_name));
+	}
+#else
+
+	if (twitter_option_add_link_to_tweet(account) && is_tweet && tweet_id)
+	{
+		PurpleConnection *gc = purple_account_get_connection(account);
+		TwitterConnectionData *twitter = gc->proto_data;
+		gchar *url = twitter_mb_prefs_get_status_url(twitter->mb_prefs, src_user, tweet_id);
+		if (url)
+		{
+			g_string_append_printf(tweet, "\n%s\n", url);
+			g_free(url);
+		}
+	}
+#endif
+
+	g_free(linkified_message);
+	return g_string_free(tweet, FALSE);
+}
+
+
+gchar *twitter_utf8_find_last_pos(const gchar *str, const gchar *needles, glong str_len)
+{
+	gchar *last;
+	const gchar *needle;
+	for (last = g_utf8_offset_to_pointer(str, str_len); last; last = g_utf8_find_prev_char(str, last))
+		for (needle = needles; *needle; needle++)
+			if (*last == *needle)
+			{
+				return last;
+			}
+	return NULL;
+}
+
+char *twitter_utf8_get_segment(const gchar *message, int max_len, const gchar *add_text, const gchar **new_start)
+{
+	int add_text_len = 0;
+	int index_add_text = -1;
+	char *status;
+	int len_left;
+	int len = 0;
+	static const gchar *spaces = " \r\n";
+
+	while (message[0] == ' ')
+		message++;
+
+	if (message[0] == '\0')
+		return NULL;
+
+	if (add_text)
+	{
+		gchar *message_lower = g_utf8_strdown(message, -1);
+		char *pnt_add_text = strstr(message_lower, add_text);
+		add_text_len = g_utf8_strlen(add_text, -1);
+		if (pnt_add_text)
+			index_add_text = g_utf8_pointer_to_offset(message, pnt_add_text) + add_text_len;
+		g_free(message_lower);
+	}
+
+	len_left = g_utf8_strlen(message, -1);
+	if (len_left <= max_len && (!add_text || index_add_text != -1))
+	{
+		status = g_strdup(message);
+		len = strlen(message);
+	} else if (len_left <= max_len && len_left + add_text_len + 1 <= max_len) {
+		status = g_strdup_printf("%s %s", add_text, message);
+		len = strlen(message);
+	} else {
+		gchar *space;
+		if (add_text 
+			&& index_add_text != -1
+			&& index_add_text <= max_len
+			&& (space = twitter_utf8_find_last_pos(message + index_add_text, spaces, max_len - g_utf8_pointer_to_offset(message, message + index_add_text)))
+			&& (g_utf8_pointer_to_offset(message, space) <= max_len))
+		{
+			//split already has our word in it
+			len = space - message;
+			status = g_strndup(message, len);
+			len++;
+		} else if ((space = twitter_utf8_find_last_pos(message, spaces, max_len - (add_text ? add_text_len + 1 : 0)))) {
+			len = space - message;
+			space[0] = '\0';
+			status = add_text ? g_strdup_printf("%s %s", add_text, message) : g_strdup(message);
+			space[0] = ' ';
+			len++;
+		} else if (index_add_text != -1 && index_add_text <= max_len) {
+			//one long word, which contains our add_text
+			char prev_char;
+			gchar *end_pos;
+			end_pos = g_utf8_offset_to_pointer(message, max_len);
+			len = end_pos - message;
+			prev_char = end_pos[0];
+			end_pos[0] = '\0';
+			status = g_strdup(message);
+			end_pos[0] = prev_char;
+		} else {
+			char prev_char;
+			gchar *end_pos;
+			end_pos = (index_add_text != -1 && index_add_text <= max_len ?
+					g_utf8_offset_to_pointer(message, max_len) :
+					g_utf8_offset_to_pointer(message, max_len - (add_text ? add_text_len + 1 : 0)));
+			end_pos = g_utf8_offset_to_pointer(message, max_len - (add_text ? add_text_len + 1 : 0));
+			len = end_pos - message;
+			prev_char = end_pos[0];
+			end_pos[0] = '\0';
+			status = add_text ? g_strdup_printf("%s %s", add_text, message) : g_strdup(message);
+			end_pos[0] = prev_char;
+		}
+	}
+	if (new_start)
+		*new_start = message + len;
+	return g_strstrip(status);
+}
+GArray *twitter_utf8_get_segments(const gchar *message, int segment_length, const gchar *add_text)
+{
+	GArray *segments;
+	const gchar *new_start = NULL;
+	const gchar *pos;
+	gchar *segment = twitter_utf8_get_segment(message, segment_length, add_text, &new_start);
+	segments = g_array_new(FALSE, FALSE, sizeof(char *));
+	while (segment)
+	{
+		g_array_append_val(segments, segment);
+		pos = new_start;
+		segment = twitter_utf8_get_segment(pos, segment_length, add_text, &new_start);
+	};
+	return segments;
+}
